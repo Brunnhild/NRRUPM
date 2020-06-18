919e3b588d26bff1ffa6ba873b94e62c92a13a67
Brunnhild <10175101259@stu.ecnu.edu.cn>
2020-06-19 01:29:29 +0800

    Start to add another layer of LSTM

--- data.py
+++ data.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+wordVec_dim = 200
+
+def filtering(word, stop_words):
+    filter_list = '.\n0123456789!,.' #去掉特殊符号
+    flag = False
+    for j in filter_list:
+        if j in word:
+            flag = True
+    if flag:
+        return True
+                
+    if word in stop_words:
+        print(word)
+        return True
 
 def get_vocabulary():
     vocabulary = dict()
@@ -21,9 +35,63 @@ def get_vocabulary():
                 vocabulary[line[:fi]] = vector
     return vocabulary
 
-#
-def get_train_input(vocabulary):
+def get_sentence(stop_words, vocabulary):
+    file_path = "yelp-2013/yelp-2013-seg-20-20.train.ss"
+    file = open(file_path, encoding = "utf-8")
+
+    #得到最长的句子
     max_len = 0
+    lengest_str = ""
+    for i, line in enumerate(file):
+        user_id, product_id, score, comment_str = line.split('\t\t')
+        comment_sentences = comment_str.split('<sssss>')
+        for sentence in comment_sentences:
+            #print(sentence)
+            temp_len = 0
+            sentence_words = sentence.split(' ')
+            for word in sentence_words:
+                if filtering(word, stop_words):
+                    continue
+                temp_len += 1
+            if temp_len > max_len:
+                max_len = temp_len
+                lengest_str = sentence
+    print(max_len)
+
+    file = open(file_path, encoding = "utf-8")
+    # 将每句话都构造成词向量矩阵
+    train_input = []
+    to_fill = []
+    for i in range(wordVec_dim):
+        to_fill.append(float(0))
+            
+    for i, line in enumerate(file):
+        user_id, product_id, score, comment_str = line.split('\t\t')
+        comment_sentences = comment_str.split('<sssss>')
+        for sentence in comment_sentences:
+            sentence_matrix = []
+            sentence_words = sentence.split(' ')
+            for word in sentence_words:
+                if filtering(word, stop_words):
+                    continue
+                if vocabulary.__contains__(word):
+                    sentence_matrix.append(vocabulary[word])
+            for i in range(max_len - len(sentence_matrix)):
+                sentence_matrix.append(to_fill)
+            train_input.append((user_id, product_id, score, sentence_matrix))
+    return train_input
+            
+#
+def get_train_input(vocabulary): #
+    #获取停用词
+    stop_path = "./stop_words.txt"
+    stop_file = open(stop_path, encoding = "utf-8")
+    stop_words = []
+    for i, word in enumerate(stop_file):
+        stop_words.append(word)
+    train_input = get_sentence(stop_words, vocabulary)
+    '''    
+    max_len = 0    
     with open('yelp-2013/yelp-2013-seg-20-20.train.ss', encoding='UTF-8') as f:
         # 第一次遍历先得到最长句子长度
         for (idx, line) in enumerate(f):
@@ -31,20 +99,25 @@ def get_train_input(vocabulary):
             comment_str = comment_str.split(' ')
             len_count = 0
             for i in comment_str:
-                if i == '<sssss>':
+                if i == '<sssss>': #去掉句子结尾符
                     continue
-                filter_list = '.\n0123456789!,.'
+                filter_list = '.\n0123456789!,.' #去掉特殊符号
                 flag = False
                 for j in filter_list:
                     if j in i:
                         flag = True
                 if flag:
                     continue
+                
+                if i in stop_words:
+                    print(i)
+                    continue
+                
                 len_count += 1
                 
             if len_count > max_len:
                 max_len = len_count
-        #print(max_len)  
+        print(max_len)  
             
     with open('yelp-2013/yelp-2013-seg-20-20.train.ss', encoding='UTF-8') as f:
         train_input = []
@@ -66,4 +139,6 @@ def get_train_input(vocabulary):
             for i in range(max_len - len(comment)):
                 comment.append(to_fill)
             train_input.append((user_id, product_id, score, comment))
+    '''
     return train_input
+    
--- main.py
+++ main.py
@@ -12,8 +12,9 @@ if __name__ == '__main__':
     X = []
     y = []
     cnt = 0
+    
     for (user_id, product_id, score, comment) in train_input:
-        if len(comment) != 1034:
+        if len(comment) != 656:
             cnt += 1
             continue
         X.append(comment)
@@ -23,16 +24,12 @@ if __name__ == '__main__':
     X = np.reshape(X, (X.shape[0], len(X[0]), 200))
     y = np.array(y)
     y = np_utils.to_categorical(y)
-    # model = Sequential()
-    # model.add(Bidirectional(LSTM(32, input_shape=(1034, 200))))
-    # model.add(Dense(6, activation='softmax'))
-    # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    # model.fit(X, y, epochs=500, batch_size=30, verbose=2)
 
     text_input = Input(shape=(1034, 200))
 
     lstm_1 = LSTM(32)(text_input)
-    lstm_2 = Dense(6)(lstm_1)
+    lstm_2 = Bidirectional()(lstm_1)
+    lstm_3 = Dense(6)(lstm_2)
 
     cnn_1 = Conv1D(5, 3, padding='valid', activation='relu', strides=1)(text_input)
     cnn_2 = Dropout(0.5)(cnn_1)
@@ -42,7 +39,7 @@ if __name__ == '__main__':
     cnn_6 = Dropout(0.5)(cnn_5)
     cnn_7 = Dense(6)(cnn_6)
 
-    combine = concatenate([lstm_2, cnn_7], axis=-1)
+    combine = concatenate([lstm_3, cnn_7], axis=-1)
     output = Dense(6, activation='softmax')(combine)
 
     model = Model(text_input, output)
